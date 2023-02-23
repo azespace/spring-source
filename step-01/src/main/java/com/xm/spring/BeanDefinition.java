2b28ece048777e5f9ac67bd9b8dbf64f512426a2
XMStory <1521682541@qq.com>
2023-02-23 14:44:50 +0800

    Initial commit

---
+++ step-01/src/main/java/com/xm/spring/BeanDefinition.java
@@ -0,0 +1,16 @@
+package com.xm.spring;
+
+/**
+ * @ClassName BeanDefinition
+ * @Description
+ * @Author XM
+ * @Date 2022/11/02 11:35
+ **/
+public class BeanDefinition {
+    private Object bean;
+
+    public BeanDefinition(Object bean){
+        this.bean = bean;
+    }
+
+}
